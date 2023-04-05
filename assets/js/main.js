e72e83b7c2cf11436315808f85f35d9e36eaf47d
Stoyan Peev <stoyanilievpeev@gmail.com>
2023-04-05 23:51:59 +0100

    feat: added animation on scroll to images and main headings

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// Hamburger Menu
 window.onload = function() {
     const menu_btn = document.querySelector('.hamburger');
     const mobile_menu = document.querySelector('.mobile-nav');
@@ -6,4 +7,23 @@ window.onload = function() {
         menu_btn.classList.toggle('active');
         mobile_menu.classList.toggle('active');
     });
+}
+
+// Scroll Animation
+const animation_elements = document.querySelectorAll('.animate-on-scroll-left, .animate-on-scroll-right');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animate');
+        }  
+    })
+}, {
+    threshold: 0.5
+});
+
+for (let i = 0; i < animation_elements.length; i++) {
+    const el = animation_elements[i];
+
+    observer.observe(el);
 }
